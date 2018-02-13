233c95dacc8f55fa4b9022021e4def3f731e662d
Stephen Kang <jkkang@sfu.ca>
2018-02-13 02:08:08 -0500

    Made the sky more blue

--- shaders/skybox_fragment.glsl
+++ shaders/skybox_fragment.glsl
@@ -4,13 +4,13 @@ in vec4 worldspace_position;
 
 out vec4 outputColor;
 
-const vec4 skytop = vec4(0.0f, 0.0f, 1.0f, 1.0f);
-const vec4 skyhorizon = vec4(0.3294f, 0.92157f, 1.0f, 1.0f);
+const vec4 skytop = vec4(0.49f, 0.75f, 0.92f, 1.0f);
+const vec4 skyhorizon = vec4(0.895f, 0.945f, 0.98f, 1.0f);
 
 void main()
 {
     vec4 pointOnSphere = vec4(normalize(worldspace_position.xyz), 1);
     float a = pointOnSphere.y;
-    float i = 0.5;
+    float i = 0.9;
     outputColor = i * mix(skyhorizon, skytop, a);
 }
